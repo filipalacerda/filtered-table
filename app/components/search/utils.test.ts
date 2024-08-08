e77349570de18350d6bbecd9c21b0e3f0d723eb4
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-08-08 16:40:00 +0100

    Adds unit test to operators filter utility function

--- app/components/search/utils.test.ts
+++ app/components/search/utils.test.ts
@@ -1,22 +1,159 @@
+import { Operator } from "@/app/types/types";
 import { filterOperators } from "./utils";
+import datastore from "@/app/data/data";
 
 describe("Search utils", () => {
-  describe("filterOperators", () => {
-    describe("when property is string", () => {
-      it("should return [equals, has any value, has no valye, is any of, contains]", () => {});
+  const operators = datastore.getOperators();
+  describe(filterOperators.name, () => {
+    describe("when no property is provided", () => {
+      it("should return an empty array", () => {
+        const result = filterOperators(operators);
+        expect(result).toEqual([]);
+      });
+    });
 
-      it("should not contain [Is greater than, is less than]", () => {});
+    describe("when property is string", () => {
+      let result: Operator[];
+
+      beforeEach(() => {
+        result = filterOperators(operators, "string");
+      });
+
+      it("should return [equals, has any value, has no valye, is any of, contains]", () => {
+        expect(result).toEqual([
+          {
+            text: "Equals",
+            id: "equals",
+          },
+          {
+            text: "Has any value",
+            id: "any",
+          },
+          {
+            text: "Has no value",
+            id: "none",
+          },
+          {
+            text: "Is any of",
+            id: "in",
+          },
+          {
+            text: "Contains",
+            id: "contains",
+          },
+        ]);
+      });
+
+      it("should not contain [Is greater than, is less than]", () => {
+        expect(result).toEqual(
+          expect.not.arrayContaining([
+            {
+              text: "Is greater than",
+              id: "greater_than",
+            },
+            {
+              text: "Is less than",
+              id: "less_than",
+            },
+          ])
+        );
+      });
     });
 
     describe("when property is number", () => {
-      it("should return [equals, is greater than, is less than, has any value, has no value, is any of]", () => {});
-      it("should not contain [Contains]", () => {});
+      let result: Operator[];
+
+      beforeEach(() => {
+        result = filterOperators(operators, "number");
+      });
+
+      it("should return [equals, is greater than, is less than, has any value, has no value, is any of]", () => {
+        expect(result).toEqual([
+          {
+            text: "Equals",
+            id: "equals",
+          },
+          {
+            text: "Is greater than",
+            id: "greater_than",
+          },
+          {
+            text: "Is less than",
+            id: "less_than",
+          },
+          {
+            text: "Has any value",
+            id: "any",
+          },
+          {
+            text: "Has no value",
+            id: "none",
+          },
+          {
+            text: "Is any of",
+            id: "in",
+          },
+        ]);
+      });
+
+      it("should not contain [Contains]", () => {
+        expect(result).toEqual(
+          expect.not.arrayContaining([
+            {
+              text: "Contains",
+              id: "contains",
+            },
+          ])
+        );
+      });
     });
 
     describe("when property is enumerated", () => {
-      it("should return [equals, has any value, has no value, is any of]", () => {});
+      let result: Operator[];
 
-      it("should not contain [contains, is less than, is greater than]", () => {});
+      beforeEach(() => {
+        result = filterOperators(operators, "enumerated");
+      });
+
+      it("should return [equals, has any value, has no value, is any of]", () => {
+        expect(result).toEqual([
+          {
+            text: "Equals",
+            id: "equals",
+          },
+          {
+            text: "Has any value",
+            id: "any",
+          },
+          {
+            text: "Has no value",
+            id: "none",
+          },
+          {
+            text: "Is any of",
+            id: "in",
+          },
+        ]);
+      });
+
+      it("should not contain [contains, is less than, is greater than]", () => {
+        expect(result).toEqual(
+          expect.not.arrayContaining([
+            {
+              text: "Contains",
+              id: "contains",
+            },
+            {
+              text: "Is greater than",
+              id: "greater_than",
+            },
+            {
+              text: "Is less than",
+              id: "less_than",
+            },
+          ])
+        );
+      });
     });
   });
 });
