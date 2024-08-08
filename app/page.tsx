04fd51d5053294ff601276d22d54233a02e93a55
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-08-08 12:31:28 +0100

    Adds filter function for products

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,25 @@
 import { useEffect, useMemo, useState } from "react";
 import datastore from "./data/data";
 
-import type { Products, Operator, Property } from "./types/types";
+import type { Products, Operator, Property, Filters } from "./types/types";
 
-export default function Home() {
+// TODO: Extract logic into it's own file and add the filters
+const filterProducts = (products: Products, filters?: Filters) => {
+  if (filters) {
+    return products;
+  } else {
+    return products;
+  }
+};
+
+const Home = () => {
   // datastore values
   const [products, setProducts] = useState<Products>([]);
   const [operators, setOperators] = useState<Operator[]>([]);
   const [properties, setProperties] = useState<Property[]>([]);
+
+  // user updated values
+  const [filters, setFilters] = useState<Filters>();
 
   useEffect(() => {
     setProducts(datastore.getProducts());
@@ -17,7 +29,14 @@ export default function Home() {
     setProperties(datastore.getProperties());
   }, [setProducts, setOperators, setProperties]);
 
+  const visibleProducts = useMemo(
+    () => filterProducts(products, filters),
+    [products, filters]
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
-}
+};
+
+export default Home;
