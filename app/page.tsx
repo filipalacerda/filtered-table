3ae6e34982809fee4ee8c29efc2a25cac345249a
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-08-08 12:28:36 +0100

    Fetches the data and stores it using useSate hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+import datastore from "./data/data";
+
+import type { Products, Operator, Property } from "./types/types";
 
 export default function Home() {
+  // datastore values
+  const [products, setProducts] = useState<Products>([]);
+  const [operators, setOperators] = useState<Operator[]>([]);
+  const [properties, setProperties] = useState<Property[]>([]);
+
+  useEffect(() => {
+    setProducts(datastore.getProducts());
+    setOperators(datastore.getOperators());
+    setProperties(datastore.getProperties());
+  }, [setProducts, setOperators, setProperties]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
---
+++ app/types/types.ts
@@ -0,0 +1,27 @@
+export type Product = {
+  id: number;
+  property_values: {
+    property_id: number;
+    value: string | number;
+  }[];
+};
+
+export type Operator = {
+  text: string;
+  id: string;
+};
+
+export type Products = Product[];
+
+export type Property = {
+  id: number;
+  name: string;
+  type: string | string[];
+  values?: string[];
+};
+
+export type Filters = {
+  operator: Operator;
+  property: Property;
+  value: string;
+};
