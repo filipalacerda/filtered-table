480628c66900e5a1d62974138879e6422300036c
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-08-10 20:44:39 +0100

    Renames main component

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { filterProducts } from "./filterProducts/filterProducts";
 
 import type { Products, Operator, Property, Filters } from "./types/types";
 
-const Home = () => {
+const App = () => {
   // datastore values
   const [products, setProducts] = useState<Products>([]);
   const [operators, setOperators] = useState<Operator[]>([]);
@@ -54,4 +54,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default App;
